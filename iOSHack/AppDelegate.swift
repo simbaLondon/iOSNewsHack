a269dc1ea374812e7dd358f3082d5aa5949fd788
Michael Baldock <michael.baldock@telegraph.co.uk>
2017-02-01 10:28:14 +0000

    using cocoapods installed firebase

--- iOSHack/AppDelegate.swift
+++ iOSHack/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,6 +16,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+      
+      FIRApp.configure()
       
       let splitViewController = self.window!.rootViewController as! UISplitViewController
       let leftNavController = splitViewController.viewControllers.first as! UINavigationController
