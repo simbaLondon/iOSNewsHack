f021457ed8353558f5653158b73b2ee5fd9e14de
Michael Baldock <michael.baldock@telegraph.co.uk>
2017-02-01 12:18:56 +0000

    fix compile

--- iOSHack/AppDelegate.swift
+++ iOSHack/AppDelegate.swift
@@ -29,7 +29,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
       
       masterViewController.sentensesDelegate = detailViewController
       
-      self.getData()
+//      self.getData()
       
         return true
     }
